4daa4adf27582a127dc54442d3cdb36c1e439c03
Tomy Hsieh <git@tomy.me>
2025-11-03 00:43:54 -0800

    ✨ feat: Add subnets variables to `do-k8s-cluster`
    to support VPC-native Networking

--- do-k8s-cluster/main.tf
+++ do-k8s-cluster/main.tf
@@ -8,10 +8,12 @@ terraform {
 }
 
 resource "digitalocean_kubernetes_cluster" "the_awesome_cluster" {
-  name    = var.name
-  region  = var.region
-  version = var.k8s_version
-  tags    = [for key, value in var.tags : "${key}:${value}"]
+  name           = var.name
+  region         = var.region
+  version        = var.k8s_version
+  cluster_subnet = var.cluster_subnet
+  service_subnet = var.service_subnet
+  tags           = [for key, value in var.tags : "${key}:${value}"]
 
   node_pool {
     name       = "${var.name}-pool"
--- do-k8s-cluster/variables.tf
+++ do-k8s-cluster/variables.tf
@@ -1,28 +1,40 @@
 variable "name" {
-  type = string
+  type        = string
+  description = "The name of the Kubernetes cluster. The node pool will be named `<name>-pool`."
 }
 
-# https://docs.digitalocean.com/platform/regional-availability/
 variable "region" {
-  # e.g. "sfo3"
-  type = string
+  type        = string
+  description = "The region where the cluster will be created, See [list of all regions](https://docs.digitalocean.com/platform/regional-availability/). e.g. `sfo3`"
 }
 
-# doctl kubernetes options versions
 variable "k8s_version" {
-  # e.g. "1.33.1-do.3"
-  type    = string
-  default = "latest"
+  type        = string
+  description = "Use `doctl kubernetes options versions` to findout what versions are supported. e.g. `1.33.1-do.3`."
+  default     = "latest"
 }
 
-# https://slugs.do-api.dev/
+# 
+variable "cluster_subnet" {
+  type        = string
+  default     = "192.168.0.0/16" # Up to 512 nodes
+  description = "The CIDR block for the cluster network. This affects the maximum number of nodes that can be created in the cluster. See [this docs](https://docs.digitalocean.com/products/kubernetes/how-to/create-clusters/#create-a-cluster-with-vpc-native-networking)"
+}
+
+variable "service_subnet" {
+  type        = string
+  default     = "172.16.0.0/19" # Up to 8192 services
+  description = "The CIDR block for the service network. This affects the maximum number of services that can be created in the cluster. See [this docs](https://docs.digitalocean.com/products/kubernetes/how-to/create-clusters/#create-a-cluster-with-vpc-native-networking)"
+}
+
 variable "node_size" {
-  # e.g. "s-2vcpu-4gb"
-  type = string
+  type        = string
+  description = "The specification of the nodes. e.g. `s-2vcpu-4gb` See [list of all sizes](https://slugs.do-api.dev/)"
 }
 
 variable "node_count" {
-  type = number
+  type        = number
+  description = "The number of nodes to create in the default node pool"
 }
 
 variable "tags" {
